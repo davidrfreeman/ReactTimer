2bf402ffbf8e8b88bb22e558fdc7446fdbd5f943
David Freeman <davidfreeman@holos.solutions>
2016-09-13 08:49:54 -0600

    created a Clock component and new file structure for testing

---
+++ app/components/Clock.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+export default class Clock extends React.Component {
+  formatSeconds(totalSeconds) {
+    let seconds = totalSeconds % 60;
+    let minutes = Math.floor(totalSeconds / 60);
+
+    if(seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    if(minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    return minutes + ':' + seconds;
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import $ from 'jQuery';
+import TestUtils from 'react-addons-test-utils';
+
+import Clock from 'Clock';
+
+describe('Clock', () => {
+  it('Should exist', () => {
+    expect(Clock).toExist();
+  });
+
+  describe('formatSeconds', ()=> {
+    it('Should format seconds', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock/>);
+
+      let seconds = 615;
+      let expected = '10:15';
+
+      let actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+
+    it('Should format seconds when min/sec are less than 10', () => {
+      let clock = TestUtils.renderIntoDocument(<Clock/>);
+
+      let seconds = 61;
+      let expected = '01:01';
+
+      let actual = clock.formatSeconds(seconds);
+
+      expect(actual).toBe(expected);
+    });
+  });
+});
