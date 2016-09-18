f0d7cb86d8a437cf527ecb63307066ddfa110c4a
David Freeman <davidfreeman@holos.solutions>
2016-09-18 15:18:26 -0600

    added method to start timer

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -7,15 +7,42 @@ export default class Countdown extends React.Component {
     super(props);
     // Bind 'this' context for handleSetCountdown method
     this.handleSetCountdown = this.handleSetCountdown.bind(this);
+    this.startTimer = this.startTimer.bind(this);
 
     this.state = {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped'
     }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    // If the state of countdownStatus has changed then we can do things based on what the countdownStatus is
+    if(this.state.countdownStatus !== prevState.countdownStatus) {
+      switch(this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      };
+    }
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      let newCount = this.state.count - 1;
+      this.setState({
+        // Checks to make sure the timer uses only positive numbers
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000)
   }
 
   handleSetCountdown(seconds) {
     this.setState({
-      count: seconds
+      // The seconds arg comes from the number entered into CountdownForm
+      count: seconds,
+      // Change countdownStatus to 'started'
+      countdownStatus: 'started'
     });
   }
 
