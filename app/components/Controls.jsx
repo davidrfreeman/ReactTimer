297aeac4257522f617c89e2832d180f87c1c5dbf
David Freeman <davidfreeman@holos.solutions>
2016-09-21 00:49:35 -0600

    Began configuring Timer component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -19,14 +19,16 @@ export default class Controls extends React.Component {
     let renderStartStopButton = () => {
       if(countdownStatus === 'started') {
         return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
-      } else if(countdownStatus === 'paused') {
+      } else {
         return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     }
 
     return (
-      <div className="controls">{renderStartStopButton()}
-      <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button></div>
+      <div className="controls">
+        {renderStartStopButton()}
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
+      </div>
     );
   }
 }
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import Clock from 'Clock';
+import Controls from 'Controls';
 
-const Timer = (props) => {
-  return (
-    <h1>Timer Component</h1>
-  );
+export default class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.startTimer = this.startTimer.bind(this);
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+
+    this.state = {
+      count: 0,
+      timerStatus: 'stopped'
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    // If the state of countdownStatus has changed then we can do things based on what the countdownStatus is
+    if(this.state.countdownStatus !== prevState.countdownStatus) {
+      switch(this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({ count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      };
+    }
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      let newCount = this.state.count + 1;
+      this.setState({
+        // Checks to make sure the timer uses only positive numbers
+        count: newCount
+      });
+
+      if(newCount === 0) {
+        this.setState({timerStatus: 'stopped'});
+      }
+    }, 1000)
+  }
+
+  handleStatusChange(newStatus) {
+    this.setState({
+      timerStatus: newStatus
+    });
+    console.log(this.state.timerStatus);
+  }
+
+  render() {
+    let { count, timerStatus } = this.state;
+
+    return (
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+      </div>
+    );
+  }
 }
-
-export default Timer;
